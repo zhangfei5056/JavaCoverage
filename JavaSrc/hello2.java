a816ec89581defeba20397985721ff7cadd8fe27
caoyuan <zhangfei5056@126.com>
2015-08-25 11:18:23 +0800

    setup file

---
+++ JavaSrc/hello2.java
@@ -0,0 +1,11 @@
+class Untitled {
+	public static void main(String[] args) {
+		String a = null;
+		System.out.println("hello world");
+		if (a == null)
+			 System.out.println("hello worlddd");
+
+		String b;
+			
+	}
+}
